04fb922d1865fefefdb25e3c774198cfdd443a23
youku_s <thebottomofwaterofhali@gmail.com>
2016-03-28 20:50:00 +0900

    Add recalculate logic

--- design/js/cs.js
+++ design/js/cs.js
@@ -2,12 +2,22 @@
   window.onload = function() {
     var on = function(targets, type, listener) {
       for (var i = 0; i < targets.length; i++){
+        targets[i].removeEventListener(type, listener);
         targets[i].addEventListener(type, listener);
       }
     }
     var deleteRow = function(e) {
       var p = e.target.parentNode.parentNode;
       p.parentNode.removeChild(p);
+    };
+    var recalc = function(target) {
+      return function() {
+        console.log(target);
+      };
+    };
+    var observe = function() {
+      var observed = document.getElementsByClassName('observed');
+      on(observed, 'change', recalc("hello!!"));
     };
     var deleteBtns = document.getElementsByClassName('deleteRow');
     on(deleteBtns, 'click', deleteRow);
@@ -19,6 +29,28 @@
       target.appendChild(clone);
       var deleteBtn = clone.querySelectorAll('.deleteRow');
       on(deleteBtn, 'click', deleteRow);
+      observe();
     });
+    var addItems = document.getElementsByClassName('addItem');
+    on(addItems, 'click', function(e) {
+      var clone = document.querySelector('table.item .hiddenRow').cloneNode(true);
+      clone.classList.remove('hiddenRow');
+      var target = document.querySelector('table.item > tbody');
+      target.appendChild(clone);
+      var deleteBtn = clone.querySelectorAll('.deleteRow');
+      on(deleteBtn, 'click', deleteRow);
+      observe();
+    });
+    var addRelations = document.getElementsByClassName('addRelation');
+    on(addRelations, 'click', function(e) {
+      var clone = document.querySelector('table.relation .hiddenRow').cloneNode(true);
+      clone.classList.remove('hiddenRow');
+      var target = document.querySelector('table.relation > tbody');
+      target.appendChild(clone);
+      var deleteBtn = clone.querySelectorAll('.deleteRow');
+      on(deleteBtn, 'click', deleteRow);
+      observe();
+    });
+    observe();
   }
 })();
