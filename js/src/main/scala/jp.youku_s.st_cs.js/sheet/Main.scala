08c17fc25f07b74666aad2653645c6559ca8e037
youku_s <thebottomofwaterofhali@gmail.com>
2016-03-28 23:33:14 +0900

    hello! Reactした

--- js/src/main/scala/jp.youku_s.st_cs.js/sheet/Main.scala
+++ js/src/main/scala/jp.youku_s.st_cs.js/sheet/Main.scala
@@ -1,12 +1,21 @@
 package jp.youku_s.st_cs.js.sheet
 
+import org.scalajs.dom.document
+
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
 
+import japgolly.scalajs.react.{ReactComponentB, ReactDOM}
+import japgolly.scalajs.react.vdom.Implicits._
+import japgolly.scalajs.react.vdom.prefix_<^.{<, ^}
+
 @JSExport
 object Main extends JSApp {
+  val NoArgs = ReactComponentB[Unit]("No args")
+      .render(_ => <.div("Hello!"))
+      .build
   @JSExport
   override def main(): Unit = {
-
+    ReactDOM.render(NoArgs(()), document.getElementById("main"))
   }
 }
