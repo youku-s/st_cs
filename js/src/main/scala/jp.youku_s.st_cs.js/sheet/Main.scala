5d2626809c2116e1b4dfd15493fb0efeec76051a
youku_s <thebottomofwaterofhali@gmail.com>
2016-03-29 09:16:29 +0900

    トップレベルのコンポーネントを作った

--- js/src/main/scala/jp.youku_s.st_cs.js/sheet/Main.scala
+++ js/src/main/scala/jp.youku_s.st_cs.js/sheet/Main.scala
@@ -2,20 +2,116 @@ package jp.youku_s.st_cs.js.sheet
 
 import org.scalajs.dom.document
 
+import scala.scalajs.js
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
 
-import japgolly.scalajs.react.{ReactComponentB, ReactDOM}
+import japgolly.scalajs.react.{BackendScope, ReactComponentB, ReactDOM, ReactElement}
 import japgolly.scalajs.react.vdom.Implicits._
 import japgolly.scalajs.react.vdom.prefix_<^.{<, ^}
 
+case class State(
+  csClass: Option[Int],
+  csType: Option[Int],
+  csEaude: Option[Int],
+  parts: Seq[Part],
+  items: Seq[Item],
+  skills: Seq[Skill],
+  relations: Seq[Relation],
+  tensions: Seq[Tension],
+  memo: String
+)
+
+case class Part(
+  name: String,
+  shihai: Int,
+  jyujyun: Int,
+  dasan: Int,
+  jyunshin: Int,
+  oshi: Int,
+  sasshi: Int,
+  koui: Int,
+  akui: Int
+)
+case class Item(
+  name: String,
+  main: Int,
+  sub: Int
+)
+case class Skill(
+  name: String,
+  timing: String,
+  cost: Int,
+  detail: String
+)
+case class Relation(
+  to: String,
+  name: String,
+  ueshita: Int,
+  semeuke: Int
+)
+case class Tension(
+  number: Option[Int],
+  ckecked: Boolean,
+  used: Boolean
+)
+
+object Tension {
+  def initial = Seq(
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(Some(5), false, false),
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(Some(10), false, false),
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(Some(15), false, false),
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(None, false, false),
+    Tension(Some(20), false, false)
+  )
+}
+
+object State {
+  def initialValue: State = State(
+    csClass = None,
+    csType = None,
+    csEaude = None,
+    parts = Seq.empty,
+    items = Seq.empty,
+    skills = Seq.empty,
+    relations = Seq.empty,
+    tensions = Tension.initial,
+    memo = ""
+  )
+}
+
+class TopBackend($: BackendScope[Unit, State]) {
+  def render(s: State): ReactElement = {
+     <.div(
+       ^.style := js.Dictionary("width" -> "80%"),
+       <.h1("少女展爛会キャラクターシート")
+     )
+  }
+}
+
 @JSExport
 object Main extends JSApp {
-  val NoArgs = ReactComponentB[Unit]("No args")
-      .render(_ => <.div("Hello!"))
-      .build
+  val top = ReactComponentB[Unit]("Top")
+    .initialState(State.initialValue)
+    .renderBackend[TopBackend]
+    .build
   @JSExport
   override def main(): Unit = {
-    ReactDOM.render(NoArgs(()), document.getElementById("main"))
+    ReactDOM.render(top(()), document.getElementById("main"))
   }
 }
